d372487085822baf4e5714534ff3639c16f6436f
saibahmedamine0-cloud <saibahmedamine0@gmail.com>
2026-05-15 02:10:14 +0100

    Update ContentView.swift

--- Sources/Sources/ContentView.swift
+++ Sources/Sources/ContentView.swift
@@ -1,1 +1,23 @@
+import SwiftUI
+import WebKit
 
+struct ContentView: View {
+    var body: some View {
+        WebView(url: URL(string: "https://m.youtube.com")!)
+            .edgesIgnoringSafeArea(.all)
+    }
+}
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        let configuration = WKWebViewConfiguration()
+        configuration.allowsInlineMediaPlayback = true
+        let webView = WKWebView(frame: .zero, configuration: configuration)
+        webView.load(URLRequest(url: url))
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {}
+}
